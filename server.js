51c71622f14aca990167badd789c9d48cc2d39d0
jchoi10 <noreply>
2022-11-05 23:04:03 -0400

    added id filter

--- server.js
+++ server.js
@@ -44,6 +44,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -54,7 +59,11 @@ app.get('/api/animals', (req, res) => {
 
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
+    if (result) {
       res.json(result);
+    } else {
+      res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
